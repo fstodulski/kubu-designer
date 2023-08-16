889012c1d2a1348e3a8527c1a70cddb57680fbb7
franciszekstodulski <franek.stodulski@gmail.com>
2023-08-16 14:34:53 +0200

    🔧 fix(PortfolioItem.svelte): remove unnecessary line breaks and indentation in the img tag

--- src/containers/Projects/components/PortfolioItem/PortfolioItem.svelte
+++ src/containers/Projects/components/PortfolioItem/PortfolioItem.svelte
@@ -8,12 +8,7 @@
 
 <div class=" flex w-full flex-col gap-2">
   <figure class="w-full overflow-hidden rounded-xl">
-    <img
-      loading="lazy"
-      class=" w-full aspect-[{aspect}]"
-      src={data.image.src}
-      alt={data.image.alt}
-    />
+    <img class=" w-full aspect-[{aspect}]" src={data.image.src} alt={data.image.alt} />
   </figure>
 
   <div class="font-inter flex items-center justify-between">
